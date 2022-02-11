534333ba62ea5cfa7c7fe4528317bf74341d935f
Leon <leopard10000@gmail.com>
2022-02-11 21:24:16 +0900

    Factory設定 ＋ faker日本語化

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(MembersTableSeeder::class);
+        \App\Models\Member::factory(100)->create();
+        // $this->call(MembersTableSeeder::class);
     }
 }
