93090add5792ee3f36a8eacfe71505dc05ba9411
Leon <leopard10000@gmail.com>
2022-02-11 14:45:46 +0900

    DB作成〜migration/seeder〜model作成まで

---
+++ database/seeders/MembersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('members')->insert([
+            [ 'name' => 'member_001' ],
+            [ 'name' => 'member_002' ],
+            [ 'name' => 'member_003' ],
+            [ 'name' => 'member_004' ],
+            [ 'name' => 'member_005' ],
+            [ 'name' => 'member_006' ],
+            [ 'name' => 'member_007' ],
+            [ 'name' => 'member_008' ],
+            [ 'name' => 'member_009' ],
+            [ 'name' => 'member_010' ]
+        ]);
+    }
+}
